14295e15127c8f5e7415733e2a2640b58773d1c4
Julie200811 <48508203+Julie200811@users.noreply.github.com>
2019-05-20 16:40:13 +0300

    Update plot4.r

--- plot4.r
+++ plot4.r
@@ -1,32 +1,40 @@
-## This first line will likely take a few seconds. Be patient!
-if(!exists("NEI")){
-  NEI <- readRDS("./data/summarySCC_PM25.rds")
-}
-if(!exists("SCC")){
-  SCC <- readRDS("./data/Source_Classification_Code.rds")
-}
-# merge the two data sets 
-if(!exists("NEISCC")){
-  NEISCC <- merge(NEI, SCC, by="SCC")
-}
+## GOAL:: Across the United States, how have emissions from coal combustion-related sources changed from 1999-2008?
 
+## read in the data
+if (!exists("NEI")) {
+      NEI <- readRDS("summarySCC_PM25.rds")
+}
+# dim(NEI)
+# str(NEI)
+# summary(NEI)
+
+if (!exists("SCC")) {
+      SCC <- readRDS("Source_Classification_Code.rds")
+}
+# dim(SCC)
+# str(SCC)
+# summary(SCC)
+
+# Subset coal combustion related NEI data
+combustionRelated <- grepl("comb", SCC$SCC.Level.One, ignore.case=TRUE)
+coalRelated <- grepl("coal", SCC$SCC.Level.Four, ignore.case=TRUE) 
+coalCombustion <- (combustionRelated & coalRelated)
+combustionSCC <- SCC[coalCombustion,]$SCC
+combustionNEI <- NEI[NEI$SCC %in% combustionSCC,]
+
+# ggplot2 plot
+dev.cur()
+png("Plot4.png", width = 750, height = 500, units = "px")
+
+if(!require(ggplot2)){
+      install.packages("ggplot2");
+      require(ggplot2)
+}
 library(ggplot2)
 
-# Across the United States, how have emissions from coal combustion-related sources changed from 1999-2008?
+ggplot(combustionNEI,aes(x = factor(year),y = Emissions/1000)) +
+      geom_bar(stat="identity", fill = combustionNEI$year, width=0.25) +
+      labs(x="year", y=expression("total PM"[2.5]*" emissions in kilotons")) + 
+      labs(title=expression("PM"[2.5]*" US CoalCombustionSource Emissions (1999-2008)"))
 
-# fetch all NEIxSCC records with Short.Name (SCC) Coal
-coalMatches  <- grepl("coal", NEISCC$Short.Name, ignore.case=TRUE)
-subsetNEISCC <- NEISCC[coalMatches, ]
-
-aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEISCC, sum)
-
-
-
-png("plot4.png", width=640, height=480)
-g <- ggplot(aggregatedTotalByYear, aes(factor(year), Emissions))
-g <- g + geom_bar(stat="identity") +
-  xlab("year") +
-  ylab(expression('Total PM'[2.5]*" Emissions")) +
-  ggtitle('Total Emissions from coal sources from 1999 to 2008')
-print(g)
 dev.off()
