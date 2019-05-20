e312610dd5e7d5605426d7bc7918dd1cd8c91a53
Julie200811 <48508203+Julie200811@users.noreply.github.com>
2019-05-20 16:33:21 +0300

    Update plot1.r

--- plot1.r
+++ plot1.r
@@ -1,16 +1,53 @@
-## This first line will likely take a few seconds. Be patient!
-if(!exists("NEI")){
-  NEI <- readRDS("./data/summarySCC_PM25.rds")
-}
-if(!exists("SCC")){
-  SCC <- readRDS("./data/Source_Classification_Code.rds")
-}
-# Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
-# Using the base plotting system, make a plot showing the total PM2.5 emission from all sources 
-# for each of the years 1999, 2002, 2005, and 2008.
+# GOAL:: Have total emissions from PM2.5 decreased in the United States from 1999 to 2008?
+# GOAL:: Using base-plot showing the total PM2.5 emission from all sources for each of the years 1999, 2002, 2005, and 2008.
 
-aggregatedTotalByYear <- aggregate(Emissions ~ year, NEI, sum)
+# Load the data
+if (!exists("NEI")) {
+      NEI <- readRDS("summarySCC_PM25.rds")
+}
+# dim(NEI)
+# str(NEI)
+# summary(NEI)
 
-png('plot1.png')
-barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' emissions at various years'))
+if (!exists("SCC")) {
+      SCC <- readRDS("Source_Classification_Code.rds")
+}
+# dim(SCC)
+# str(SCC)
+# summary(SCC)
+
+# count the total emissions by year from NEI dataset
+Emissions_by_year <- aggregate(Emissions~year, NEI, sum)
+str(Emissions_by_year)
+
+# converting Emissions in millions for better plotting results
+Emissions_by_year$EmissionsInMillions = Emissions_by_year$Emissions / 1000000
+str(Emissions_by_year)
+
+# Plot1 (BASE Plot)
+dev.cur()
+png("Plot1.png", width = 750, height = 500, units = "px")
+
+# setting the margins
+par(mar = c(5, 4, 2, 1), mfrow = c(1,2))
+
+barplot(Emissions_by_year$EmissionsInMillions,
+      names.arg = Emissions_by_year$year,
+      col = factor(Emissions_by_year$year),
+      xlab = 'Years',
+      ylab = expression('Emissions PM' [2.5]* 'in millions'),
+      main =  expression('PM'[2.5]*'Emissions/year'))
+
+plot( Emissions_by_year$EmissionsInMillions ~ Emissions_by_year$year,
+      type = "o",
+      main = expression('PM'[2.5]*'Emissions/year'),
+      xlab = "Year",
+      ylab = expression('Emissions PM' [2.5]* 'in millions'),
+      pch = 19,
+      col = "darkblue",
+      lty = 6)
+
 dev.off()
+
+
+
